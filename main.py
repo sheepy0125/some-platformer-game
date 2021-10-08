2f029eec5d1b97038d50c9dd2902c82cfabdc15a
coffeeisgood <rydenpark01@gmail.com>
2021-10-08 01:02:55 -0400

    fixed syntax error

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import pygame,sys
 screen = pygame.display.set_mode((500,500))
 
 while True:
-    for event in pygame.event.get:
+    for event in pygame.event.get():
         if event.type == pygame.QUIT:
             #exit the game
             pygame.quit()
