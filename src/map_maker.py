762468481e703867415d906a8cf130774a215622
sheepy0125 <sheepy404@gmail.com>
2021-10-10 22:12:54 -0400

    Make map maker a bit easier to use

--- src/map_maker.py
+++ src/map_maker.py
@@ -8,7 +8,6 @@ Created by duuuck and sheepy0125
 ### Setup ###
 #############
 # Import
-import pygame
 from pathlib import Path
 from pygame_setup import *
 from world import Tile, load_world, TILE_SIZE
@@ -24,40 +23,47 @@ scrolled_by = 0
 ### Functions ###
 #################
 def snap_to_grid(location) -> list:
-    return [(int(location[i] / TILE_SIZE) * TILE_SIZE) for i in range(2)]
+    return tuple([(int(location[i] / TILE_SIZE) * TILE_SIZE) for i in range(2)])
+
+
+def find_tile(tile_pos) -> bool | int:
+    """Only finds the first tile"""
+
+    for tile_idx, tile in enumerate(tiles):
+        if (tile.x - tile.scroll_x, tile.y) == tuple(tile_pos):
+            return tile_idx
+
+    return False
 
 
 def create_tile(mouse_pos):
+    tile_pos = snap_to_grid(mouse_pos)
+    if find_tile(tile_pos) is not False:
+        return
+
     tiles.append(
         Tile(
-            tile_pos := snap_to_grid(mouse_pos),
+            tile_pos,
             image_path=str(ROOT_PATH / "assets" / "images" / "tiles" / "dirt.png"),
         )
     )
-    Logger.log(f"Created tile at {tile_pos}")
+    # Logger.log(f"Created tile at {tile_pos}")
 
 
 def destroy_tile(mouse_pos):
-    # Find the tile at the mouse position
-    # Sure, I could use caching in a dict to make this process instant
-    # But, too bad!
     tile_pos = snap_to_grid(mouse_pos)
-    for tile_idx, tile in enumerate(tiles):
-        if (tile.x - tile.scroll_x, tile.y) == tuple(tile_pos):
-            # That's the one!
-            Logger.log(f"Removed the tile at {tile_pos}")
-            tiles.pop(tile_idx)
-            return
-
-    Logger.fatal(f"No tile found at {tile_pos}")
+    if (tile_idx := find_tile(tile_pos)) is not False:
+        tiles.pop(tile_idx)
 
 
 def scroll_screen(multiplier: int):
+    if scrolled_by <= 0:
+        return False
+
     for tile in tiles:
         tile.scroll_x += multiplier * TILE_SIZE
-        if tile.scroll_x < 0:
-            tile.scroll_x = 0
-    # Logger.log(f"Scrolled tiles by {multiplier}. New scrolled by is {scrolled_by}")
+
+    return True
 
 
 def export():
@@ -69,7 +75,7 @@ def export():
 ############
 texts = [
     Text("Map Maker for Some Platformer Game", size=12, pos=(SCREEN_SIZE[0] // 2, 15)),
-    Text("Press H to hide this text", size=12, pos=(SCREEN_SIZE[0] // 2, 30)),
+    Text("Press H to toggle this text", size=12, pos=(SCREEN_SIZE[0] // 2, 30)),
     Text("Press the arrow keys to scroll", size=12, pos=(SCREEN_SIZE[0] // 2, 45)),
 ]
 currently_scrolling_text = Text(
@@ -86,18 +92,6 @@ while True:
             pygame.quit()
             exit(0)
 
-        # Mouse click
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            mouse_x, mouse_y = pygame.mouse.get_pos()
-
-            # Creating
-            if event.button == 1:
-                create_tile((mouse_x, mouse_y))
-
-            # Destroying
-            else:
-                destroy_tile((mouse_x, mouse_y))
-
         # Keypress
         if event.type == pygame.KEYUP:
             # Toggle text
@@ -106,13 +100,13 @@ while True:
 
             # Scroll screen to the right
             if event.key == pygame.K_RIGHT:
-                scrolled_by += TILE_SIZE
                 scroll_screen(1)
+                scrolled_by += TILE_SIZE
 
             # Scroll screen to the left
             elif event.key == pygame.K_LEFT:
-                scrolled_by -= TILE_SIZE
-                scroll_screen(-1)
+                if scroll_screen(-1):
+                    scrolled_by -= TILE_SIZE
 
             # Not scrolling
             else:
@@ -125,6 +119,17 @@ while True:
                 pos=(SCREEN_SIZE[0] // 2, 60),
             )
 
+    # Mouse click
+    if (buttons_pressed := pygame.mouse.get_pressed()) != (0, 0, 0):
+        mouse_x, mouse_y = pygame.mouse.get_pos()
+
+        # Creating
+        if buttons_pressed[0]:
+            create_tile((mouse_x, mouse_y))
+
+        # Destroying
+        else:
+            destroy_tile((mouse_x, mouse_y))
     # Draw
     screen.fill("blue")
     for tile in tiles:
@@ -134,4 +139,4 @@ while True:
             text.draw()
         currently_scrolling_text.draw()
     pygame.display.update()
-    clock.tick(15)
+    clock.tick(60)
