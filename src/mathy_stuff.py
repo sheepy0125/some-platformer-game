3ce582a5064d63b9177f19aa6020fb877799d9f3
sheepy0125 <sheepy404@gmail.com>
2021-10-22 20:52:23 -0400

    Format mathy stuff

--- src/mathy_stuff.py
+++ src/mathy_stuff.py
@@ -1,12 +1,13 @@
+# duuuuck, what the hell is this?
 
 
-def ccw(A,B,C):
-    return (C[1]-A[1]) * (B[0]-A[0]) > (B[1]-A[1]) * (C[0]-A[0])
+def ccw(A, B, C):
+    return (C[1] - A[1]) * (B[0] - A[0]) > (B[1] - A[1]) * (C[0] - A[0])
+
 
 # Return true if line segments AB and CD intersect
-def intersect(A,B,C,D):
+def intersect(A, B, C, D):
     if A == C or A == D or B == C or B == D:
         return True
 
-
-    return ccw(A,C,D) != ccw(B,C,D) and ccw(A,B,C) != ccw(A,B,D)
+    return ccw(A, C, D) != ccw(B, C, D) and ccw(A, B, C) != ccw(A, B, D)
