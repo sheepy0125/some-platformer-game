2da7889e9b9b054b61f0d2b648cd87d78cc3a141
sheepy0125 <sheepy404@gmail.com>
2021-10-23 14:00:34 -0400

    Line intersections

--- src/mathy_stuff.py
+++ src/mathy_stuff.py
@@ -4,23 +4,36 @@ Created by duuuck and sheepy0125
 22/10/2021
 """
 
-# Write a function that finds if two points are in counter clockwise order
+
 def ccw(A, B, C):
-    """
-    Check if three points are in counter clockwise order
-    """
+    """Check if three points are in counter clockwise order"""
     return (C[1] - A[1]) * (B[0] - A[0]) > (B[1] - A[1]) * (C[0] - A[0])
 
 
-# Write a function that finds the cross product of two vectors
-# The cross product is a vector perpendicular to both vectors
-# A vector is represented by a tuple with two integers
-# Use the ccw function to check if the points are in counter clockwise order
-def cross_product(A, B):
-    """
-    Find the cross product of two vectors
-    """
-    if ccw(A, B, (0, 0)):
-        return (A[0] * B[1] - A[1] * B[0], A[1] * B[0] - A[0] * B[1])
-    else:
-        return (A[0] * B[1] + A[1] * B[0], A[1] * B[0] + A[0] * B[1])
+def line_intersection(line1, line2):
+    """Find the intersection between two lines"""
+
+    # Check if the lines are in counter clockwise order
+    if ccw(line1[0], line1[1], line2[0]) == ccw(line1[0], line1[1], line2[1]):
+        return None
+    if ccw(line2[0], line2[1], line1[0]) == ccw(line2[0], line2[1], line1[1]):
+        return None
+
+    # Calculate the slope of the lines
+    m1 = (line1[1][1] - line1[0][1]) / (line1[1][0] - line1[0][0])
+    m2 = (line2[1][1] - line2[0][1]) / (line2[1][0] - line2[0][0])
+
+    # Calculate the y-intercepts of the lines
+    b1 = line1[0][1] - m1 * line1[0][0]
+    b2 = line2[0][1] - m2 * line2[0][0]
+
+    # Calculate the x-intercepts of the lines
+    x = (b2 - b1) / (m1 - m2)
+    y = m1 * x
+
+    # Debug print in slope intercept form
+    print(f"Line 1: y={m1}x + {b1}")
+    print(f"Line 2: y={m2}x + {b2}")
+    print(f"Intersection: ({x}, {y})")
+
+    return (x, y)
