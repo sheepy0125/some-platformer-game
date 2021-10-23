df2421ab310199d936998de288f3b6ea4692f748
sheepy0125 <sheepy404@gmail.com>
2021-10-23 04:26:45 -0400

    Idk what a cross product is but I think Github Copilot does

--- src/mathy_stuff.py
+++ src/mathy_stuff.py
@@ -1,13 +1,26 @@
-# duuuuck, what the hell is this?
+"""
+Mathy stuff for Some Platformer Game
+Created by duuuck and sheepy0125
+22/10/2021
+"""
 
-
+# Write a function that finds if two points are in counter clockwise order
 def ccw(A, B, C):
+    """
+    Check if three points are in counter clockwise order
+    """
     return (C[1] - A[1]) * (B[0] - A[0]) > (B[1] - A[1]) * (C[0] - A[0])
 
 
-# Return true if line segments AB and CD intersect
-def intersect(A, B, C, D):
-    if A == C or A == D or B == C or B == D:
-        return True
-
-    return ccw(A, C, D) != ccw(B, C, D) and ccw(A, B, C) != ccw(A, B, D)
+# Write a function that finds the cross product of two vectors
+# The cross product is a vector perpendicular to both vectors
+# A vector is represented by a tuple with two integers
+# Use the ccw function to check if the points are in counter clockwise order
+def cross_product(A, B):
+    """
+    Find the cross product of two vectors
+    """
+    if ccw(A, B, (0, 0)):
+        return (A[0] * B[1] - A[1] * B[0], A[1] * B[0] - A[0] * B[1])
+    else:
+        return (A[0] * B[1] + A[1] * B[0], A[1] * B[0] + A[0] * B[1])
