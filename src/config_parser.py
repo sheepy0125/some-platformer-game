f9f0dcada8e69cd1956260c6a31b8401a3391c81
sheepy0125 <sheepy404@gmail.com>
2021-10-25 18:51:08 -0400

    Playtest in map maker and new map!

--- src/config_parser.py
+++ src/config_parser.py
@@ -6,6 +6,7 @@ Created by sheepy0125
 
 from json import load
 from utils import Logger, ROOT_PATH
+from sys import argv
 
 try:
     CONFIG_FILE_PATH = str(ROOT_PATH / "config.json")
@@ -15,6 +16,15 @@ try:
     FPS = int(config_dict["fps"])
     GRAVITY = float(config_dict["gravity"])
 
+    # Get the first map to play if it is in the env
+    MAP_PATH = ROOT_PATH / "src" / "maps"
+    if len(argv) > 1:
+        MAP_PATH /= argv[1]
+    else:
+        MAP_PATH /= "0-test.map"
+    MAP_PATH = str(MAP_PATH)
+
+
 except Exception as error:
     Logger.log_error(error)
     exit(1)
@@ -23,5 +33,6 @@ del CONFIG_FILE_PATH
 del config_dict
 del config_file
 del load
+del argv
 
 Logger.log("Successfully loaded config")
